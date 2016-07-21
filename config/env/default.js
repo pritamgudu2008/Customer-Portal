3fba4950b808ef7de576866719b25e5cef1672c6
PKMROCKSS <PKMROCKSS>
2016-07-21 08:05:20 +0530

    First Checkin

--- config/env/default.js
+++ config/env/default.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
+    title: 'Customer Portal',
     description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
@@ -11,6 +11,6 @@ module.exports = {
   templateEngine: 'swig',
   sessionSecret: 'MEAN',
   sessionCollection: 'sessions',
-  logo: 'modules/core/img/brand/logo.png',
-  favicon: 'modules/core/img/brand/favicon.ico'
+  logo: 'modules/core/img/brand/policy.jpg',
+  favicon: 'modules/core/img/brand/Couple-icon.png'
 };
